ec13694036b9d42a44f38d14e8f2b2543ce6fccc
paddygu <wwwhappygxd@163.com>
2017-12-04 10:15:57 +0800

    2222

--- ofo-demo/ViewController.swift
+++ ofo-demo/ViewController.swift
@@ -16,7 +16,6 @@ class ViewController: UIViewController {
         //修改导航栏item的主题颜色，否则将会把背景图片按照默认的蓝色主题渲染。发现下面两句对storyboard设置的item并不起作用，所以将自己手动写代码设置左右item背景图片及主题色。
         //self.navigationItem.leftBarButtonItem?.image?.withRenderingMode(.alwaysOriginal)
         //self.navigationItem.rightBarButtonItem?.image?.withRenderingMode(.alwaysOriginal)
-        
         self.navigationItem.leftBarButtonItem?.image = #imageLiteral(resourceName: "leftTopImage").withRenderingMode(.alwaysOriginal)
         self.navigationItem.rightBarButtonItem?.image = #imageLiteral(resourceName: "rightTopImage").withRenderingMode(.alwaysOriginal)
         
